935c4165f048ca02627fd932c0e8c2ed785a8ede
Aswath K <aswath.sana@gmail.com>
2018-10-13 16:37:40 +0530

    Fixed build errors
    Signed-off-by: Aswath K <aswath.sana@gmail.com>

--- utils/k8s/openebscrd.go
+++ utils/k8s/openebscrd.go
@@ -44,22 +44,18 @@ func (k8s K8S) DeleteStoragePoolClaim(spcName string) error {
 // GetCStorPool returns the CStorPool object for given cStorPoolName.
 // :return: *openebs_v1.CStorPool: Pointer to CStorPool objects.
 func (k8s K8S) GetCStorPool(cStorPoolName string) (*openebs_v1.CStorPool, error) {
-	cStorPoolClient := k8s.OpenebsClientSet.Openebs().CStorPools()
+	cStorPoolClient := k8s.OpenebsClientSet.OpenebsV1alpha1().CStorPools()
 	return cStorPoolClient.Get(cStorPoolName, meta_v1.GetOptions{})
 }
 
 // ListCStorPool returns all CStorPool objects.
-func (k8s K8S) ListCStorPool() ([]openebs_v1.CStorPool, error) {
-	cStorPoolClient := k8s.OpenebsClientSet.Openebs().CStorPools()
-	cStorPools, err := cStorPoolClient.List(meta_v1.ListOptions{})
-	if err != nil {
-		return nil, err
-	}
-	return GetCStorPools.Items, nil
-} 
+func (k8s K8S) ListCStorPool() (*openebs_v1.CStorPoolList, error) {
+	cStorPoolClient := k8s.OpenebsClientSet.OpenebsV1alpha1().CStorPools()
+	return cStorPoolClient.List(meta_v1.ListOptions{})
+}
 
 // DeleteCStorPool deletes a CStorPool with the given name.
 func (k8s K8S) DeleteCStorPool(cStorPoolName string) error {
-	cStorePoolClient := k8s.OpenebsClientSet.Openebs().CStorePools()
+	cStorePoolClient := k8s.OpenebsClientSet.OpenebsV1alpha1().CStorPools()
 	return cStorePoolClient.Delete(cStorPoolName, &meta_v1.DeleteOptions{})
 }
